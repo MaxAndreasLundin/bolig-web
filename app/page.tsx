690a1f3a15c78296cd2ee7ce8776c0dfb9fc7949
juliosc1 <julio.siklander@gmail.com>
2022-12-12 13:48:52 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
+        <h1>Bolig</h1>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js 13!</a>
         </h1>
