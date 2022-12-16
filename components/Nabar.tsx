7f5d1b6a379490ef2e31a2ea215cb6691c8f72b3
juliosc1 <julio.siklander@gmail.com>
2022-12-16 08:59:54 +0100

    add login icon

--- components/Nabar.tsx
+++ components/Nabar.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { MenuIcon, XIcon } from "@heroicons/react/solid";
+import { UserIcon } from "@heroicons/react/outline";
 import Link from "next/link";
 import React, { useState } from "react";
 
@@ -15,19 +16,22 @@ const Navbar = () => {
     <header className="bg-gray-400 sticky top-0 h-20 z-50 grid grid-cols-2 p-4">
       {/*left*/}
       <div className="flex items-center">
-        <h1 className="w-full text-3xl">Bolig</h1>
+        <h1 className="w-full text-3xl p-2">Bolig</h1>
       </div>
 
       {/*right*/}
       <div className="flex items-center justify-end">
-        <ul className="hidden md:flex">
-          <li className="p-4">Home</li>
-          <li className="p-4">Home</li>
-          <li className="p-4">Home</li>
-          <li className="p-4">Home</li>
+        <ul className="hidden md:flex md:text-lg lg:text-xl">
+          <li className="p-4">Sälja_bostad</li>
+          <li className="p-4">Sök_mäklare</li>
+          <li className="p-4">Nyheter</li>
+          <li className="p-4">Kontakt</li>
         </ul>
-        <div className="block md:hidden" onClick={handleClick}>
-          {click ? <XIcon className="h-6" /> : <MenuIcon className="h-6" />}
+        <div className="flex gap-2 border-2 p-2 sm:p-1 rounded-full">
+          <UserIcon className="h-6" />
+          <div className="block md:hidden" onClick={handleClick}>
+            {click ? <XIcon className="h-6" /> : <MenuIcon className="h-6" />}
+          </div>
         </div>
         {/*mobile */}
         <div
@@ -39,10 +43,10 @@ const Navbar = () => {
         >
           <h1 className="w-full text-3xl m-4">Bolig</h1>
           <ul className="p-4">
-            <li className="p-4 border-b">Home</li>
-            <li className="p-4 border-b">Home</li>
-            <li className="p-4 border-b">Home</li>
-            <li className="p-4 border-b">Home</li>
+            <li className="p-4 border-b">Sälja_bostad</li>
+            <li className="p-4 border-b">Sök_mäklare</li>
+            <li className="p-4 border-b">Nyheter</li>
+            <li className="p-4 border-b">Kontakt</li>
           </ul>
         </div>
       </div>
