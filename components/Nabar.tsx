f99a293e80f30a96ae7d87d49b3888ef316dcb3e
juliosc1 <julio.siklander@gmail.com>
2022-12-16 11:33:31 +0100

    add styling in navbar

--- components/Nabar.tsx
+++ components/Nabar.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { MenuIcon, XIcon } from "@heroicons/react/solid";
 import { UserIcon } from "@heroicons/react/outline";
-import Link from "next/link";
 import React, { useState } from "react";
 
 const Navbar = () => {
@@ -11,25 +10,25 @@ const Navbar = () => {
     setClick(!click);
   };
 
-  //{click ? "bg-slate-500 flex flex-col items-center absolute right-0 top-16 h-56 w-full transition duration-500 z-10" : "web"}
   return (
-    <header className="bg-gray-400 sticky top-0 h-20 z-50 grid grid-cols-2 p-4">
+    <header className="bg-gray-400 sticky top-0 h-20 z-50 grid grid-cols-4">
       {/*left*/}
-      <div className="flex items-center">
-        <h1 className="w-full text-3xl p-2">Bolig</h1>
+      <div className="flex items-center col-span-1">
+        <h1 className="w-full text-3xl pl-2">Bolig</h1>
       </div>
 
       {/*right*/}
-      <div className="flex items-center justify-end">
-        <ul className="hidden md:flex md:text-lg lg:text-xl">
-          <li className="p-4">Sälja_bostad</li>
-          <li className="p-4">Sök_mäklare</li>
-          <li className="p-4">Nyheter</li>
-          <li className="p-4">Kontakt</li>
+      <div className="flex items-center justify-end col-span-3 pr-2">
+        <ul className="hidden sm:flex md:text-lg lg:text-xl cursor-pointer">
+          <li className="p-4 inline-block">Sälja bostad</li>
+          <li className="p-4 inline-block">Sök mäklare</li>
+          <li className="p-4 inline-block">Nyheter</li>
+          <li className="p-4 inline-block">Kontakt</li>
         </ul>
-        <div className="flex gap-2 border-2 p-2 sm:p-1 rounded-full">
+        <div className="flex items-center gap-2 border-2 p-2 sm:px-3 sm:p-1 rounded-full">
+          <p className="hidden sm:flex md:text-lg lg:text-xl">Logga in</p>
           <UserIcon className="h-6" />
-          <div className="block md:hidden" onClick={handleClick}>
+          <div className="block sm:hidden cursor-pointer" onClick={handleClick}>
             {click ? <XIcon className="h-6" /> : <MenuIcon className="h-6" />}
           </div>
         </div>
@@ -37,14 +36,14 @@ const Navbar = () => {
         <div
           className={
             click
-              ? "fixed right-0 top-20 w-[50%] h-full border-r border-r-red-900 bg-gray-600 ease-in-out duration-500 md:hidden"
+              ? "fixed right-0 top-20 w-[50%] h-full border-r border-r-red-900 bg-gray-600 ease-in-out duration-500 sm:hidden"
               : "fixed right-[-100%] top-20 h-full  w-[50%] transition-all duration-500"
           }
         >
           <h1 className="w-full text-3xl m-4">Bolig</h1>
-          <ul className="p-4">
-            <li className="p-4 border-b">Sälja_bostad</li>
-            <li className="p-4 border-b">Sök_mäklare</li>
+          <ul className="p-4 cursor-pointer" onClick={() => setClick(false)}>
+            <li className="p-4 border-b">Sälja bostad</li>
+            <li className="p-4 border-b">Sök mäklare</li>
             <li className="p-4 border-b">Nyheter</li>
             <li className="p-4 border-b">Kontakt</li>
           </ul>
