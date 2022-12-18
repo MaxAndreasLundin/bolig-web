6103fe8cf80b63cc163b8147c95ef42d3dcdf571
juliosc1 <julio.siklander@gmail.com>
2022-12-18 19:38:10 +0100

    add styling in form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,8 +6,8 @@ const login = () => {
     <div className='bg-white flex flex-col items-center justify-center h-[90vh]'>
       <h1 className='text-black text-2xl font-bold'>Logga in</h1>
       <div className='flex gap-4 m-8'>
-        <input className='border border-black bg-white rounded-full text-sm h-10 pl-2' type="text" name="name" placeholder='Andvändarnamn...'/>
-        <input className='border border-black bg-white rounded-full text-sm h-10 pl-2' type="password" name="password" placeholder='Lössenord...'/>
+        <input className='input-field' type="text" name="name" placeholder='Andvändarnamn...'/>
+        <input className='input-field' type="password" name="password" placeholder='Lössenord...'/>
       </div>
       <p className='text-black' >Inget konto? Skapa ett konto genom att <Link href={"registration"}><span className='underline cursor-pointer'>klicka här!</span></Link></p>
     </div>
--- app/registration/page.tsx
+++ app/registration/page.tsx
@@ -1,22 +1,49 @@
-import React from 'react'
+import React from "react";
 
 const registration = () => {
   return (
-    <div className='flex flex-col'>
-      <form action="">
-        <h1>Registrera dig</h1>
-        <div>
-          <input type="text" placeholder='Namn'/>
-          <input type="text" placeholder='Efternamn'/>
+    <div className="bg-white flex flex-col items-center justify-center h-[90vh]">
+      <form action="" className="bg-slate-200 p-10 rounded-lg">
+        <h1 className="flex justify-center mb-10 text-black text-2xl font-bold underline">
+          Registrera dig
+        </h1>
+        <div className="flex gap-4 mb-8">
+          <input
+            type="text"
+            placeholder="Namn"
+            className="input-field text-black"
+          />
+          <input
+            type="text"
+            placeholder="Efternamn"
+            className="input-field text-black"
+          />
         </div>
-        <input type="email" placeholder='Email'/>
-        <div>
-          <input type="text" placeholder='Användarnamn'/>
-          <input type="password" placeholder='Lössenord'/>
+        <div className="flex justify-center mb-8">
+          <input
+            type="email"
+            placeholder="Email"
+            className="input-field w-full text-black"
+          />
         </div>
+        <div className="flex gap-4 mb-8">
+          <input
+            type="text"
+            placeholder="Användarnamn"
+            className="input-field text-black"
+          />
+          <input
+            type="password"
+            placeholder="Lössenord"
+            className="input-field text-black"
+          />
+        </div>
+        <button className="bg-green-500 flex justify-center w-full rounded-full p-2 border border-black hover:bg-green-600">
+          <p className="hover:scale-110">Registrera dig!</p>
+        </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default registration
+export default registration;
