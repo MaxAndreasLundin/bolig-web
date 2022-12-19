2fc74c0b30b77c66549631a17a35fc8cfa27ef8e
juliosc1 <julio.siklander@gmail.com>
2022-12-19 13:14:41 +0100

    add form with formik

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,17 +1,33 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const login = () => {
   return (
-    <div className='bg-white flex flex-col items-center justify-center h-[90vh]'>
-      <h1 className='text-black text-2xl font-bold'>Logga in</h1>
-      <div className='flex gap-4 m-8'>
-        <input className='input-field' type="text" name="name" placeholder='Andvändarnamn...'/>
-        <input className='input-field' type="password" name="password" placeholder='Lössenord...'/>
+    <div className="bg-white flex flex-col items-center justify-center h-[90vh]">
+      <h1 className="text-black text-2xl font-bold">Logga in</h1>
+      <div className="flex gap-4 m-8">
+        <input
+          className="input-field"
+          type="text"
+          name="name"
+          placeholder="Andvändarnamn..."
+        />
+        <input
+          className="input-field"
+          type="password"
+          name="password"
+          placeholder="Lössenord..."
+        />
       </div>
-      <p className='text-black' >Inget konto? Skapa ett konto genom att <Link href={"registration"}><span className='underline cursor-pointer'>klicka här!</span></Link></p>
+      <p className="text-black">
+        Inget konto? Skapa ett konto genom att{" "}
+        <Link href={"registration"}>
+          <span className="underline cursor-pointer">klicka här!</span>
+        </Link>
+      </p>
+      <p className="text-blue-500 text-xs pt-2">Glömt lössenord, klicka här.</p>
     </div>
-  )
-}
+  );
+};
 
-export default login
+export default login;
--- app/registration/page.tsx
+++ app/registration/page.tsx
@@ -1,9 +1,27 @@
+"use client";
+import { useFormik } from "formik";
 import React from "react";
 
 const registration = () => {
+
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      userName: "",
+      password: "",
+    },
+
+    onSubmit: (values) => {
+      console.log(values);
+    }
+  })
+  
+
   return (
     <div className="bg-white flex flex-col items-center justify-center h-[90vh]">
-      <form action="" className="bg-slate-200 p-10 rounded-lg">
+      <form onSubmit={formik.handleSubmit} className="bg-slate-200 p-10 rounded-lg">
         <h1 className="flex justify-center mb-10 text-black text-2xl font-bold underline">
           Registrera dig
         </h1>
@@ -11,11 +29,17 @@ const registration = () => {
           <input
             type="text"
             placeholder="Namn"
+            id="firstName"
+            value={formik.values.firstName}
+            onChange={formik.handleChange}
             className="input-field text-black"
           />
           <input
             type="text"
             placeholder="Efternamn"
+            id="lastName"
+            value={formik.values.lastName}
+            onChange={formik.handleChange}
             className="input-field text-black"
           />
         </div>
@@ -23,6 +47,9 @@ const registration = () => {
           <input
             type="email"
             placeholder="Email"
+            id="email"
+            value={formik.values.email}
+            onChange={formik.handleChange}
             className="input-field w-full text-black"
           />
         </div>
@@ -30,15 +57,24 @@ const registration = () => {
           <input
             type="text"
             placeholder="Användarnamn"
+            id="userName"
+            value={formik.values.userName}
+            onChange={formik.handleChange}
             className="input-field text-black"
           />
           <input
             type="password"
             placeholder="Lössenord"
+            id="password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
             className="input-field text-black"
           />
         </div>
-        <button className="bg-green-500 flex justify-center w-full rounded-full p-2 border border-black hover:bg-green-600">
+        <button
+          type="submit"
+          className="bg-green-500 flex justify-center w-full rounded-full p-2 border border-gray-600 hover:bg-green-600"
+        >
           <p className="hover:scale-110">Registrera dig!</p>
         </button>
       </form>
