db13355377e0d4f32e2798c50b553b200356d660
juliosc1 <julio.siklander@gmail.com>
2023-01-10 20:50:20 +0100

    grid setup

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import { FaInstagram, FaFacebookSquare, FaLinkedinIn } from "react-icons/fa"
+import React from "react";
+import { FaInstagram, FaFacebookSquare, FaLinkedinIn } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className='absolute bottom-0 w-full h-16 border-2 border-green-500'>
-      <div className='items-start border-2 border-red-500 gird gird-cols-1'>
-        <div className='flex items-center justify-center gap-2'>
+    <div className="absolute bottom-0 w-full h-16">
+      <div className="grid grid-cols-1 sm:grid-cols-4 w-full h-full">
+        <div className="flex sm:col-start-2 sm:col-span-2 justify-center items-center w-full gap-2 text-sm sm:text-base">
           <FaInstagram />
-          <p>Instagram</p>
+          <p className="pr-2">Instagram</p>
           <FaFacebookSquare />
-          <p>Facebook</p>
+          <p className="pr-2">Facebook</p>
           <FaLinkedinIn />
-          <p>pnkedin</p>
-        </div>  
-        <p>Bloig 2023</p>
+          <p>Linkedin</p>
+        </div>
+        <div className="flex sm:col-end-5 justify-center sm:justify-end items-center w-full sm:pr-6 text-sm sm:text-base">
+          &copy;Bolig 2023
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
