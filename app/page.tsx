7349caa34466ec544a88e81c399e0a3b1b4daf82
juliosc1 <julio.siklander@gmail.com>
2023-01-11 21:14:03 +0100

    add handle click function to open and close button

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,28 @@
+"use client"
 import ChatBox from "../components/ChatBox";
+import { useState } from "react";
+import { IoClose } from 'react-icons/io5';
+import { BsChatDots } from 'react-icons/bs';
 
 export default function Home() {
+  const [click, setClick] = useState(true);
+
+  const handleClick = () => {
+    setClick(!click)
+  }
+
   return (
     <div className="h-[90vh] w-full flex flex-col justify-center items-center">
       <h1 className="text-6xl font-extrabold">Homepage</h1>
-      <ChatBox />
+
+      <div className="static">
+        <button onClick={handleClick} className='absolute top-96 pt-1 z-10 right-0'>
+          {click ? <IoClose className="w-6 h-6 mr-2" /> : <BsChatDots className="w-10 h-10 mr-1 animate-bounce hover:animate-none hover:scale-105" />} 
+        </button>
+        <div className={click ? "fixed top-96 right-0 ease-in-out duration-500" : "fixed right-[-100%] transition-all duration-500"}>
+          <ChatBox />
+        </div>   
+      </div>
     </div>
   );
 }
--- components/ChatBox.tsx
+++ components/ChatBox.tsx
@@ -1,19 +1,21 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import { IoSendSharp, IoClose } from 'react-icons/io5';
 import { BsChatDots } from 'react-icons/bs';
 
 const ChatBox = () => {
+  const [click, setClick] = useState(true);
+
+  const handleClick = () => {
+    setClick(!click)
+  }
 
   return (
-    <div className='bg-neutral-800 flex flex-col w-64 h-64 border'>
+    <div className='bg-neutral-900 flex flex-col w-64 h-64 rounded-l-2xl'>
       <div className='flex justify-between px-2'>
-        <h4 className='text-lg'>Live Chat</h4>  
-        <button>
-          <IoClose />
-          <BsChatDots /> 
-        </button>      
+        <h4 className='pl-2 text-lg'>Live Chat</h4>     
       </div>
-      <div className='flex justify-end w-full h-44 gap-4 px-2 border'>
+      <div className='bg-neutral-800 flex justify-end w-full h-44 gap-4 px-2'>
         <p>messanger</p>
         <p>user</p>
       </div>
@@ -21,6 +23,7 @@ const ChatBox = () => {
         <input type="text" placeholder='Enter your question...' className='h-8 w-48 rounded-full text-sm text-black outline-0 border-none'/>
         <button className='bg-blue-800 w-8 h-8 border-2 rounded-full flex items-center justify-center'><IoSendSharp className="" /></button>
       </div>
+      
     </div>
   )
 }
