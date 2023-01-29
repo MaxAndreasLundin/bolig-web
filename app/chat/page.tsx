182275404c32d5069a6e71f270cf8827f0f3df56
juliosc1 <julio.siklander@gmail.com>
2023-01-29 16:15:36 +0100

    onSubmit function and styling

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -5,11 +5,11 @@ const page = () => {
   return (
       <div className='flex flex-1'>
         <div className='grid grid-cols-1 grid-rows-3 sm:grid-cols-2 sm:grid-rows-1 w-full'>
-          <div className='flex flex-col justify-center items-center border'>
+          <div className='flex flex-col justify-center items-center'>
             <h1 className='text-5xl font-bold'>Welcome,</h1>
             <p>Sign in or create an account to access this website.</p>
           </div>
-          <div className='flex flex-col justify-center items-center row-span-2 border'>
+          <div className='flex flex-col justify-center items-center row-span-2'>
             <FormChatLogin />
           </div>
         </div>
--- components/FormChatLogin.tsx
+++ components/FormChatLogin.tsx
@@ -1,45 +1,67 @@
-"use client"
-import React, { useState } from 'react'
-import { FaUser } from 'react-icons/fa'
+"use client";
+import React, { FormEvent, useState } from "react";
+import { FaUser } from "react-icons/fa";
 
 type LoginDataProps = {
   username: string;
   password: string;
-}
+};
 
 const defaultLogin: LoginDataProps = {
   username: "",
   password: "",
-}
+};
 
 const FormChatLogin = () => {
   const [loginInput, setLoginInput] = useState(defaultLogin);
-  const {username, password} = loginInput;
-  
+  const { username, password } = loginInput;
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setLoginInput((prevState) => ({
       ...prevState,
-      [e.target.id]: e.target.value,    
+      [e.target.id]: e.target.value,
     }));
   };
 
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoginInput(defaultLogin);
+
+    console.log(loginInput);
+  };
 
   return (
-    <form action="">
-      <div className='flex justify-center'>
-        <FaUser className='text-4xl'/>
+    <form onSubmit={onSubmit} className="border rounded-xl h-96 w-96 p-10 shadow-2xl">
+      <div className="flex justify-center">
+        <FaUser className="text-4xl" />
       </div>
-      <div className='flex flex-col'>
+      <div className="flex flex-col">
         <label htmlFor="username">Username</label>
-        <input type="text" placeholder='...' onChange={handleChange} />
+        <input
+          type="text"
+          placeholder="..."
+          id="username"
+          value={username}
+          onChange={handleChange}
+          className="text-black rounded-xl mt-2"
+        />
       </div>
-      <div className='flex flex-col'>
+      <div className="flex flex-col">
         <label htmlFor="password">Password</label>
-        <input type="password" placeholder='...' onChange={handleChange} />
+        <input
+          type="password"
+          placeholder="..."
+          id="password"
+          value={password}
+          onChange={handleChange}
+          className="text-black rounded-xl mt-2"
+        />
       </div>
-      <button type='submit' className='border rounded-xl h-10 w-16'>Login</button>
+      <button type="submit" className="border rounded-xl h-10 w-16 mt-4">
+        Login
+      </button>
     </form>
-  )
-}
+  );
+};
 
-export default FormChatLogin
+export default FormChatLogin;
