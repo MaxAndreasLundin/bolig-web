3e31a277f185a9f4d8e98a604fd1f19453d9f468
juliosc1 <julio.siklander@gmail.com>
2023-01-30 20:44:08 +0100

    add some comments

--- app/chatUserAccount/page.tsx
+++ app/chatUserAccount/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 import React, { useState } from "react";
 
+//PrivateRoute tar en prop som heter "children" och dess typ är React.ReactNode
+//(children = <div>Welcome to the Chat User Account page!</div>")
 const PrivateRoute = ({ children }: { children: React.ReactNode }) => {
   const [isLoading, setIsLoading] = useState(true);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
+  //Kontrollerar om en token finns i localStorage
   React.useEffect(() => {
     const token = localStorage.getItem("token");
     if (!token) {
@@ -12,7 +15,7 @@ const PrivateRoute = ({ children }: { children: React.ReactNode }) => {
     } else {
       setIsAuthenticated(true);
     }
-    setIsLoading(false);
+    setIsLoading(false); //när laddningen är klar sätts "isLoading" till false
   }, []);
 
   if (isLoading) {
@@ -23,6 +26,7 @@ const PrivateRoute = ({ children }: { children: React.ReactNode }) => {
     return <div>Unauthorized</div>;
   }
 
+  //returnerar "children" om både "isLoading" och "isAuthenticated" är false.
   return <div>{children}</div>;
 };
 
