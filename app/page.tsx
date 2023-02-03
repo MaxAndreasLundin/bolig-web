f662d68cfde62f131a32f44e40d5b90c71aa96a5
juliosc1 <julio.siklander@gmail.com>
2023-02-03 20:31:37 +0100

    change height setup in navbar and login page, created custom bolig bg-color

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
 
   return (
-    <div className="w-full flex flex-1 flex-col justify-center items-center bg-gradient-to-t from-[#393a97] to-[#01094c]">
+    <div className="bg-bolig w-full flex flex-1 flex-col justify-center items-center">
       <h1 className="text-6xl font-extrabold">Homepage</h1>
     </div>
   );
