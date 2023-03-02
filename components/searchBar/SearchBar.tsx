7dc61197ca6de5dedb244825b64d5d3d2b65da7f
juliosc1 <julio.siklander@gmail.com>
2023-03-02 08:39:02 +0100

    refactory the code for search bar and form

--- components/searchBar/SearchBar.tsx
+++ components/searchBar/SearchBar.tsx
@@ -2,11 +2,11 @@ import React, { ChangeEvent, useState } from "react";
 import { GoSearch } from "react-icons/go";
 import SearchForm from "./SearchForm";
 
-interface SearchDataProps {
-  typeOfLiving: string[],
-  numbOfRoom: string
-  livingArea: string
-  highestPrice: string
+export interface SearchDataProps {
+  typeOfLiving: string[];
+  numbOfRooms: string;
+  livingArea: string;
+  highestPrice: string;
 }
 
 const SearchBar = () => {
@@ -15,30 +15,28 @@ const SearchBar = () => {
   const handleSearchInput = (e: ChangeEvent<HTMLInputElement>) => {
     setSearchLocationInput(e.target.value);
   };
-  
+
   const getFormData = (searchData: SearchDataProps) => {
-    const newSearch2 = {
+    const newSearch = {
       ...searchData,
       city: searchLocationInput,
     };
-    console.log("Form Data:", newSearch2);
-    console.log("Handelform:", getFormData);
+
+    console.log("New Search:", newSearch);
   };
-  
 
   return (
-    <div className="flex flex-col items-center justify-center">
-
-      <div className="flex items-center justify-center border-2 border-blue-900 rounded-full md:shadow-sm bg-white">
+    <div className="flex w-full flex-col items-center justify-center">
+      <div className="flex items-center justify-center rounded-full border-2 border-blue-900 bg-white md:w-[80%] md:shadow-sm">
         <input
           type="text"
           value={searchLocationInput}
           onChange={handleSearchInput}
           placeholder="Search your new home..."
-          className="flex-grow bg-transparent rounded-2xl p-2  border-none sm:px-6 outline-none text-sm sm:text-base text-gray-600 placeholder:text-xs placeholder:sm:text-base"
+          className="flex-grow rounded-2xl border-none bg-transparent  p-2 text-sm text-gray-600 outline-none placeholder:text-xs sm:px-6 sm:text-base placeholder:sm:text-base"
         />
-        <div className="bg-blue-900 rounded-full md:mx-2 w-full">
-          <GoSearch className="hidden md:flex justify-center items-center p-1 m-1 text-2xl text-white h-6 w-6 " />
+        <div className="rounded-full bg-blue-900 md:mx-2">
+          <GoSearch className="m-1 hidden h-6 w-6 items-center justify-center p-1 text-2xl text-white md:flex " />
         </div>
       </div>
 
--- components/searchBar/SearchForm.tsx
+++ components/searchBar/SearchForm.tsx
@@ -4,30 +4,20 @@ import LivingArea from "./searchFormElement/LivingArea";
 import NumbOfRooms from "./searchFormElement/NumbOfRooms";
 import TypeOfLiving from "./searchFormElement/TypeOfLiving";
 import { GoSearch } from "react-icons/go";
+import { SearchDataProps } from "./SearchBar";
 
-type SearchFormProps = {
-  onSearchForm: (newSearch: {
-    typeOfLiving: string[];
-    numbOfRoom: string;
-    livingArea: string;
-    highestPrice: string;
-  }) => void;
-};
+interface SearchFormProps {
+  onSearchForm: (newSearch: SearchDataProps) => void;
+}
 
-const SearchForm = (props: SearchFormProps) => {
-  const [selectedTypesOfLiving, setSelectedTypesOfLiving] = useState<string[]>(
-    []
-  );
+const SearchForm = ({ onSearchForm }: SearchFormProps) => {
+  const [typeOfLiving, setTypeOfLiving] = useState<string[]>([]);
   const [numbOfRooms, setNumbOfRooms] = useState("15");
-  const [livingArea, setLivingArea] = useState("900");
+  const [livingArea, setLivingArea] = useState("250");
   const [highestPrice, setHighestPrice] = useState("20000000");
 
-  /* const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearchInput(e.target.value);
-  }; */
-
   const handleTypeOfLiving = (selectedHousing: string[]) => {
-    setSelectedTypesOfLiving(selectedHousing);
+    setTypeOfLiving(selectedHousing);
   };
 
   const handleNumbOfRooms = (typeOfRoom: string) => {
@@ -46,34 +36,34 @@ const SearchForm = (props: SearchFormProps) => {
     e.preventDefault();
 
     const newSearch = {
-      typeOfLiving: selectedTypesOfLiving,
-      numbOfRoom: numbOfRooms,
-      livingArea: livingArea,
-      highestPrice: highestPrice,
+      typeOfLiving,
+      numbOfRooms,
+      livingArea,
+      highestPrice,
     };
-    props.onSearchForm(newSearch);
+    onSearchForm(newSearch);
   };
 
   return (
     <form
       onSubmit={onSubmit}
-      className="flex flex-col rounded-2xl bg-blue-50 text-gray-600"
+      className="flex w-full flex-col rounded-2xl bg-blue-50 pt-6 text-gray-600 lg:w-[80%]"
     >
       <div className="">
         <TypeOfLiving onTypeOfLivingSelected={handleTypeOfLiving} />
       </div>
-      <div className="border-b  pb-6 md:pb-8 mb-6 md:mb-8">
+      <div className="mb-6  border-b pb-6 md:mb-8 md:pb-8 lg:w-[40%]">
         <NumbOfRooms onSelectRooms={handleNumbOfRooms} />
         <LivingArea onLivingArea={handleLivingArea} />
         <HighestPrice onHighestPrice={handleHighestPrice} />
       </div>
 
-      <div className="flex justify-center items-center gap-8 mb-4">
-        <button className="border rounded-md py-2 px-4 md:px-10">Cancel</button>
+      <div className="mb-4 flex items-center justify-center gap-8">
+        <button className="rounded-md border py-2 px-4 md:px-10">Cancel</button>
 
         <button
           type="submit"
-          className="flex items-center justify-center gap-2 rounded-md bg-blue-900 p-2 md:px-8 text-white hover:bg-blue-800"
+          className="flex items-center justify-center gap-2 rounded-md bg-blue-900 p-2 text-white hover:bg-blue-800 md:px-8"
         >
           Search <GoSearch />
         </button>
