fec60f491b8c1b6006b8fa109bacc4c25cc0823d
juliosc1 <julio.siklander@gmail.com>
2023-04-03 11:13:23 +0200

    add search bar and loading function

--- app/page.tsx
+++ app/page.tsx
@@ -6,35 +6,42 @@ export default function Home() {
   return (
     <div className="flex h-[100vh] w-[100vw] flex-1 flex-col items-center text-indigo-900">
       <div
-        className="min-h-[600px] w-full max-w-[1200px] lg:rounded-xl border bg-cover bg-center bg-no-repeat"
+        className="min-h-[600px] w-full max-w-[1200px] border bg-cover bg-center bg-no-repeat lg:rounded-xl"
         style={{
           backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.2), rgba(255, 255, 255, 0.2)), url(/houseParis2.jpg)`,
         }}
       >
-        <div className="h-full w-full md:w-[50%] lg:w-[40%] flex flex-1 flex-col items-start justify-center pt-10 p-4 bg-white bg-opacity-80 md:bg-transparent">
-          <div className='mb-20'>
-            <p className="text-3xl font-semibold md:font-bold drop-shadow-2xl md:text-4xl">
-              A New Home Awaits, <br/> Find It Now!
+        <div className="flex h-full w-full flex-1 flex-col items-start justify-center bg-white bg-opacity-80 p-4 pt-10 md:w-[50%] md:bg-transparent lg:w-[40%]">
+          <div className="mb-20">
+            <p className="text-3xl font-semibold drop-shadow-2xl md:text-4xl md:font-bold">
+              A New Home Awaits, <br /> Find It Now!
             </p>
-            <p className='text-base font-semibold tracking-wider py-2'>Discover Exceptional Real Estate Opportunities Across the Nation.</p>
-            <p>Welcome to Bolig, the premier destination for all your real estate needs.</p>
-            <p>Browse through our exclusive listings, tailored to meet a wide range of preferences and budgets.</p>
+            <p className="py-2 text-base font-semibold tracking-wider">
+              Discover Exceptional Real Estate Opportunities Across the Nation.
+            </p>
+            <p>
+              Welcome to Bolig, the premier destination for all your real estate
+              needs.
+            </p>
+            <p>
+              Browse through our exclusive listings, tailored to meet a wide
+              range of preferences and budgets.
+            </p>
           </div>
-          
-            <SearchBar />
+
+          <SearchBar />
 
           {/* <GetListOfAllResidence /> */}
         </div>
-
       </div>
 
-      <div className='flex flex-1 flex-col gap-4 text-white'>
-        <p className='text-indigo-900 font-semibold'>newest Delas</p>
-        <div className='flex gap-4'>
-          <div className='bg-blue-800 w-52 h-52 rounded-lg'>1</div>
-          <div className='bg-blue-800 w-52 h-52 rounded-lg'>2</div>
-          <div className='bg-blue-800 w-52 h-52 rounded-lg'>3</div>
-          <div className='bg-blue-800 w-52 h-52 rounded-lg'>4</div>
+      <div className="flex flex-1 flex-col gap-4 text-white">
+        <p className="font-semibold text-indigo-900">newest Delas</p>
+        <div className="flex gap-4">
+          <div className="h-52 w-52 rounded-lg bg-blue-800">1</div>
+          <div className="h-52 w-52 rounded-lg bg-blue-800">2</div>
+          <div className="h-52 w-52 rounded-lg bg-blue-800">3</div>
+          <div className="h-52 w-52 rounded-lg bg-blue-800">4</div>
         </div>
       </div>
     </div>
--- app/residenceForSale/page.tsx
+++ app/residenceForSale/page.tsx
@@ -2,40 +2,61 @@
 import React, { useState, useEffect } from "react";
 import ResidenceCard from "../../components/card/ResidenceCard";
 import { ResidenceListProps } from "../../components/helpers/ResidenceList";
+import SearchBar from "../../components/searchBar/SearchBar";
 
 const ResidenceForSale = () => {
   const [searchResult, setSearchResult] = useState<ResidenceListProps[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const storedResult = localStorage.getItem("searchResult");
     if (storedResult) {
       setSearchResult(JSON.parse(storedResult));
+      setIsLoading(false)
+    } else {
+      setIsLoading(false)
     }
   }, []);
 
   return (
-    <div className="bg-white text-gray-500">
-      <div className="my-10 border-b p-2">
-        <h1 className="pb-4 text-4xl font-semibold">Residence for sale</h1>
+    <>
+    <div className="flex h-[100vh] w-[100vw] flex-1 justify-center text-gray-500 border border-red-700">
 
-        <div>
-          {searchResult.map((item) => (
-            <div key={item.id}>
-              <ResidenceCard
-                id={item.id}
-                title={item.title}
-                city={item.city}
-                description={item.description}
-                typeOfResidence={item.typeOfResidence}
-                price={item.price}
-                room={item.room}
-                area={item.area}
-              />
-            </div>
-          ))}
-        </div>
+      {isLoading ? (
+        <div className="flex justify-center items-center">
+        <p className="text-2xl">Loading...</p>
       </div>
+      ) : (
+      <div className="flex flex-col md:flex-row justify-center items-center w-full max-w-[1200px] border-2">
+        <div className="flex md:hidden items-center justify-center w-full pt-6 px-8"><SearchBar /></div>
+        <div className="hidden md:flex w-[500px] border">Search</div>
+        
+        <div className="w-full my-10 border p-2">
+          <h1 className="pb-4 text-4xl font-semibold">Residence for sale</h1>
+
+          <div className="w-full">
+            {searchResult.map((item) => (
+              <div key={item.id}>
+                <ResidenceCard
+                  id={item.id}
+                  title={item.title}
+                  city={item.city}
+                  description={item.description}
+                  typeOfResidence={item.typeOfResidence}
+                  price={item.price}
+                  room={item.room}
+                  area={item.area}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+
+      </div>
+
+      )}
     </div>
+    </>
   );
 };
 
