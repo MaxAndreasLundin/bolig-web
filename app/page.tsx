92f14f8caf136355b1cfebffb9d61a56408b4dc3
juliosc1 <julio.siklander@gmail.com>
2023-04-06 18:04:37 +0200

    card slider + button

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,21 @@
 'use client';
+import { useState } from 'react';
 import GetListOfAllResidence from '../components/searchBar/GetListOfAllResidence';
 import SearchBar from '../components/searchBar/SearchBar';
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai';
 
 export default function Home() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const totalSlides = 8;
+  const imagesToShow = 4;
+
+  const handlePrevSlide = () => {
+    setCurrentSlide((prev) => (prev === 0 ? 0 : prev - 1));
+  };
+
+  const handleNextSlide = () => {
+    setCurrentSlide((prev) => prev + 1);
+  };
   return (
     <div className="flex h-[100vh] w-[100vw] flex-1 flex-col items-center text-indigo-900">
       <div
@@ -36,13 +49,25 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="flex flex-1 flex-col w-full max-w-[1200px] gap-4 text-white bg-[#f6f4fc] border">
-        <p className="font-semibold text-indigo-900">newest Delas</p>
-        <div className="flex gap-4">
-          <div className="h-52 w-52 rounded-lg bg-blue-800">1</div>
-          <div className="h-52 w-52 rounded-lg bg-blue-800">2</div>
-          <div className="h-52 w-52 rounded-lg bg-blue-800">3</div>
-          <div className="h-52 w-52 rounded-lg bg-blue-800">4</div>
+      <div className="flex flex-col items-center w-full max-w-[1200px] gap-4 text-white bg-[#f6f4fc] py-10 border">
+        <p className="font-bold text-indigo-900 text-2xl pl-4 py-4">Newest Delas</p>
+        <div className="flex justify-center items-center w-[80%] overflow-hidden gap-4 border">
+          <button onClick={handlePrevSlide} className='bg-gradient-to-r from-[#7d6af5] to-[#9c4ecf] rounded-full p-2'>
+            <AiOutlineArrowLeft className='text-white'/>
+          </button>
+          {Array.from({ length: totalSlides }).map((_, index) => (
+            <div
+              key={index}
+              className={`h-52 w-52 rounded-lg bg-blue-800 transition-all duration-500 ${
+                index >= currentSlide && index < currentSlide + imagesToShow ? 'block' : 'hidden'
+              }`}
+            >
+              {index + 1}
+            </div>
+          ))}
+          <button onClick={handleNextSlide} disabled={currentSlide === totalSlides - imagesToShow} className='bg-gradient-to-r from-[#7d6af5] to-[#9c4ecf] rounded-full p-2' >
+            <AiOutlineArrowRight className='text-white'/>
+          </button>
         </div>
       </div>
     </div>
