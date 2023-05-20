28fd67ea7701b4a4d89b71c62239a51e212e3986
juliosc1 <julio.siklander@gmail.com>
2023-05-20 16:02:52 +0200

    create three news letter in helpers

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className='flex h-[100vh] w-[100vw] flex-1 justify-center text-primary'>
+      <div className='border flex flex-col w-full max-w-[1400px] bg-white_bolig p-10'>
+        <div className='w-full'>
+          <h2 className='text-3xl font-bold tracking-wide'>Real Estate News</h2>
+          <p>Latest news from the housing market. Here we gather everything from interior design tips to guides and economic questions.</p>
+        </div>
+        <div>news</div>
+      </div>
+    </div>
   )
 }
 
---
+++ components/helpers/NewsLetter.tsx
@@ -0,0 +1,5 @@
+export const NewsLetter = [
+  {id: 1, title: "breaking News", info: "lorem ipsu"},
+  {id: 2, title: "breaking News", info: "lorem ipsu"},
+  {id: 3, title: "breaking News", info: "lorem ipsu"},
+]
