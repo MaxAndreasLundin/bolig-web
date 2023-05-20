36a2ddf5fd1c77165f6eb49eeaf0794c2ff9e219
juliosc1 <julio.siklander@gmail.com>
2023-05-20 17:25:45 +0200

    create breaking news section

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,23 +1,41 @@
 import React from 'react'
 import { NewsLetter } from '../../components/helpers/NewsLetter'
 import NewsLetterCard from '../../components/card/NewsLetterCard'
+import { BreakingNewsLetter } from '../../components/helpers/BreakingNewsLetter'
+import { HiOutlineNewspaper } from 'react-icons/hi'
 
 const page = () => {
   return (
     <div className='flex h-[100vh] w-[100vw] flex-1 justify-center text-primary'>
-      <div className='border flex flex-col w-full max-w-[1400px] bg-white_bolig p-10'>
-        <div className='w-full'>
-          <h2 className='text-3xl font-bold tracking-wide'>Real Estate News</h2>
-          <p>Latest news from the housing market. Here we gather everything from interior design tips to guides and economic questions.</p>
+      <div className='flex flex-col w-full max-w-[1400px] bg-white_bolig p-10'>
+        
+        <div className='w-full pb-4'>
+          <h2 className='text-3xl font-bold tracking-wide pb-4'>Real Estate News</h2>
+          <p className='text-lg'>Latest news from the housing market. Here we gather everything from interior design tips to guides and economic questions.</p>
         </div>
-        <div>{NewsLetter.map((news) => (
-          <NewsLetterCard 
-            id={news.id}
-            title={news.title}
-            info={news.info}
-            image={news.image}
-          />
-        ))}</div>
+        
+        <div className='flex justify-between'>
+          <div>{NewsLetter.map((news) => (
+            <NewsLetterCard 
+              id={news.id}
+              title={news.title}
+              info={news.info}
+              image={news.image}
+            />
+          ))}</div>
+          <div className='border-l-2 border-third hidden lg:flex flex-col w-[30%] p-4'>
+            <div className='flex justify-center items-center gap-4 pb-6'>
+              <h3 className='text-3xl font-semibold'>Breaking News</h3>
+              <HiOutlineNewspaper className='text-2xl text-third'/>
+            </div>
+            <ul>
+              {BreakingNewsLetter.map((newsTitle) => (
+                <li key={newsTitle.id} className='font-medium tracking-wide py-4 border-b-2 hover:cursor-pointer'>{newsTitle.title}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      
       </div>
     </div>
   )
---
+++ components/helpers/BreakingNewsLetter.tsx
@@ -0,0 +1,9 @@
+export const BreakingNewsLetter = [
+  {id: 1, title: "Digital Transactions: The New Normal in Real Estate"},
+  {id: 2, title: "Rising Mortgage Rates: What This Means for Home Buyers"},
+  {id: 3, title: "The Future is Green: Eco-friendly Homes on the Rise"},
+  {id: 4, title: "Commercial Real Estate: Navigating the Post-Pandemic Landscape"},
+  {id: 5, title: "Exploring the Tiny House Movement: A Shift Toward Minimalist Living"},
+  {id: 6, title: "Rental Market Reacts: The Effects of Short-Term Rentals on Housing Availability"},
+  {id: 7, title: "Luxury Real Estate Market: A Surge in Demand for High-End Properties"},
+]
